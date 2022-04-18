0c7e82c04ef2b76e1c6a6da3346e5eb890823280
Akanksha Singh <Akanksha.Singh@Triconinfotech.com>
2022-04-18 18:01:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pdfjs-viewer';
+
+  @ViewChild('pdfViewer', { static: true }) public pdfViewer: any;
+
+  constructor(private http: HttpClient){
+
+  }
+  private downloadFile(url: string): any {
+    return this.http.get(url, { responseType: 'blob' })
+        .pipe(
+            map((result: any) => {
+                return result;
+            })
+        );
+   }
+
+   ngOnInit(){
+
+    let url = "https://pdfjs-express.s3-us-west-2.amazonaws.com/docs/choosing-a-pdf-viewer.pdf";
+    this.downloadFile(url).subscribe(
+    (res:any) => {
+        this.pdfViewer.pdfSrc = res; // pdfSrc can be Blob or Uint8Array
+        this.pdfViewer.refresh(); // Ask pdf viewer to load/reresh pdf
+      }
+    );
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { PdfJsViewerModule } from 'ng2-pdfjs-viewer';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    PdfJsViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
